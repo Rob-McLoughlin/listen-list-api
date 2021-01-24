d6e0ff38ddbec4b74213e801a1354021398b4624
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2021-01-24 00:03:00 +0000

    feat(spotify): added search fn

--- tests/test_utils_spotify.py
+++ tests/test_utils_spotify.py
@@ -10,3 +10,13 @@ def test_sy_get_token():
   assert 'access_token' in token
   with pytest.raises(ValueError):
     utils_spotify.sy_get_token('', client_secret)
+
+def test_sy_search():
+  client_id = utils.keys('sy_client_id')
+  client_secret = utils.keys('sy_client_secret')
+  token = utils_spotify.sy_get_token(client_id, client_secret)
+  search = utils_spotify.sy_search('Kid A', token)
+  assert type(search) == dict
+  album_results = search['albums']['items']
+  album_names = [album['name'] for album in album_results]
+  assert 'Kid A' in album_names
--- utils_spotify.py
+++ utils_spotify.py
@@ -3,6 +3,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 import utils
 import base64
+import urllib.parse
+from pprint import pprint
 
 def sy_get_token(client_id: str, client_secret: str) -> dict:
   """Gets a Spotify token
@@ -18,13 +20,38 @@ def sy_get_token(client_id: str, client_secret: str) -> dict:
   url = 'https://accounts.spotify.com/api/token'
   data = {'grant_type': 'client_credentials'}
   r = requests.post(url, data, auth=HTTPBasicAuth(client_id, client_secret))
-  print(r.status_code, r.text)
   if r.status_code == 200:
     return r.json()
   else:
     raise ValueError(f"{r.status_code}, {r.text}")
 
+def sy_search(term: str, key: dict):
+  """Searches spotify for albums and artists
+
+  Args:
+      term (str): The search term
+      key (dict): A key object
+  """
+  query = {
+    "q": term,
+    "type": 'album,artist'
+  }
+  url = 'https://api.spotify.com/v1/search?{}'.format(urllib.parse.urlencode(query))
+  headers = {'Authorization': f"Bearer {key['access_token']}"}
+  r = requests.get(url, headers=headers)
+  if r.status_code == 200:
+    return r.json()
+  else:
+    raise ValueError(f"{r.status_code}, {r.text}")
+
+
 if __name__ == '__main__':
   client_id = utils.keys('sy_client_id')
   client_secret = utils.keys('sy_client_secret')
-  print(sy_get_token(client_id, client_secret))
+  token = sy_get_token(client_id, client_secret)
+  results = sy_search('Kid A', token)
+  albums = [album for album in results['albums']['items']]
+  artists = [artist for artist in results['artists']['items']]
+  print(f"There are {len(albums)} albums and {len(artists)} artists found")
+  # print('Artists:', [artist['name'] for artist in artist])
+  # print(results)
