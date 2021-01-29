23ba6a4187fc5297679c5905641af031f06d3783
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2021-01-29 18:59:58 +0000

    feat(dynamo): function to retrieve list from db

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,3 +13,28 @@ def test_keys():
   assert type(utils.keys()) == dict
   assert type(utils.keys('cog_app_id')) == str
   assert utils.keys('abc') == None
+
+
+## DYNAMO TESTS
+def test_create_listen_list():
+  albums = [
+    {
+      "album_id": 123,
+      "artist_id": 123,
+      "artist_title": "Radiohead",
+      "album_title": "Kid A",
+      "spotify_link": "https://open.spotify.com/album/6GjwtEZcfenmOf6l18N7T7?si=im-0p4eEQz2Nz6zM6JprLA",
+      "spotify_album_id": 123,
+      "spotify_artist_id": 123,
+      "rating": 9,
+      "listened_to": False
+    }
+  ]
+  owner_id = 0
+  title = 'Listen List 1'
+  new_list = utils.create_ll(owner_id, title, albums)
+  assert new_list != False
+  new_id = new_list['list_id']
+  # Make sure the new list is in the db
+  db_list = utils.get_ll(new_id)
+  assert type(db_list) == dict
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 import boto3
+from boto3.dynamodb.types import TypeDeserializer
+from botocore.exceptions import ClientError
 from datetime import datetime
 import uuid
 
@@ -27,7 +29,7 @@ def keys(key=None) -> dict:
         return None
     return data
 
-def create_ll(owner_id: int, title: str, albums: list) -> bool:
+def create_ll(owner_id: int, title: str, albums: list) -> dict:
   """Creates a new listen list in the database
 
   Args:
@@ -36,7 +38,7 @@ def create_ll(owner_id: int, title: str, albums: list) -> bool:
       albums (list): List of album objects
 
   Returns:
-      bool: Success
+      dict: The list item that was created
   """
   item = {
     "list_id": str(uuid.uuid4()),
@@ -51,23 +53,72 @@ def create_ll(owner_id: int, title: str, albums: list) -> bool:
   response = table.put_item(Item=item)
   print(response)
   if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-    return True
+    return item
   return False
 
+def dynamo_to_dict(dynamo_response: dict) -> dict:
+  """Takes a dynamo response and turns it into a dict
+
+  Args:
+      dynamo_response (dict): A dynamo schema object
+
+  Returns:
+      dict: Dict without the data types
+  """
+  obj = {}
+  keys = dynamo_response.keys()
+  for key in keys:
+    attribute = dynamo_response[key]
+    obj[key] = attribute
+  return obj
+
+
+def get_ll(list_id: str) -> dict:
+  """Returns a listen list from the database
+
+  Args:
+      list_id (str): The id of the list to return
+
+  Returns:
+      dict: The listen list
+  """
+  db = boto3.resource('dynamodb')
+  table = db.Table('ListenList')
+  key = {
+    'list_id': list_id,
+  }
+  try:
+    response = table.get_item(Key=key)
+    item = response['Item']
+    # print(item)
+    data = dynamo_to_dict(item)
+    return data
+    # print(item.keys())
+    # To go from low-level format to python
+    # return deserialised
+    # deserialised = {k: deserialiser.deserialize(v) for k, v in response.get("Item").items()}
+    # print(deserialised)
+    # TODO: Add not found/error responses
+  except ClientError as err:
+    raise err
+
+
 if __name__ == '__main__':
-  albums = [
-    {
-      "album_id": 123,
-      "artist_id": 123,
-      "artist_title": "Radiohead",
-      "album_title": "Kid A",
-      "spotify_link": "https://open.spotify.com/album/6GjwtEZcfenmOf6l18N7T7?si=im-0p4eEQz2Nz6zM6JprLA",
-      "spotify_album_id": 123,
-      "spotify_artist_id": 123,
-      "rating": 9,
-      "listened_to": False
-    }
-  ]
-  owner_id = 0
-  title = 'Listen List 1'
-  create_ll(owner_id, title, albums)
+  # albums = [
+  #   {
+  #     "album_id": 123,
+  #     "artist_id": 123,
+  #     "artist_title": "Radiohead",
+  #     "album_title": "Kid A",
+  #     "spotify_link": "https://open.spotify.com/album/6GjwtEZcfenmOf6l18N7T7?si=im-0p4eEQz2Nz6zM6JprLA",
+  #     "spotify_album_id": 123,
+  #     "spotify_artist_id": 123,
+  #     "rating": 9,
+  #     "listened_to": False
+  #   }
+  # ]
+  # owner_id = 0
+  # title = 'Listen List 1'
+  # create_ll(owner_id, title, albums)
+  ll = get_ll('248c917e-a9ce-47a2-8c28-73fa594452e2')
+  print(ll['albums'])
