4317614ab04ee120c1cf1256f58ed65f55ab146e
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2021-02-08 22:27:13 +0000

    feat(accounts): added fn to delete accounts

--- utils_user.py
+++ utils_user.py
@@ -1,12 +1,12 @@
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, ParamValidationError
 import utils
 import json
 
 
 def sign_up(email: str, password: str):
     client = boto3.client("cognito-idp")
-    response = {"success": False}
+    response = {"success": False, "details": ""}
     try:
         attempt = client.sign_up(
             ClientId=utils.keys("cog_client_id"),
@@ -17,8 +17,9 @@ def sign_up(email: str, password: str):
         response['details'] = attempt
     except ClientError as e:
         response['details'] = e.response['Error']['Message']
+    except ParamValidationError as e:
+        response['details'] = str(e)
     finally:
-      print(response)
       return response
 
 
@@ -33,12 +34,30 @@ def sign_in(username: str, password: str):
     )
     return response
 
+def delete_account(email: str) -> dict:
+    """Deletes a user
+
+    Args:
+        email (str): The email to delete
+
+    Returns:
+        dict: success and details
+    """
+    client = boto3.client("cognito-idp")
+    response = {"success": False, "details": ""}
+    try:
+        attempt = client.admin_delete_user(
+            UserPoolId=utils.keys('cog_pool_id'),
+            Username=email
+        )
+        response['success'] = True
+        response['details'] = attempt
+    except ClientError as e:
+        response['details'] = e.response['Error']['Message']
+    except ParamValidationError as e:
+        response['details'] = str(e)
+    finally:
+      return response
 
 if __name__ == "__main__":
-    email = "robbiemcloughlin@gmail.com"
-    username = "rob2"
-    pw = "Password1@"
-    # sign_up(username, email, pw)
-    response = sign_in(username, pw)
-    with open("key.json", "w") as outfile:
-        json.dump(response, outfile, indent=2)
+    pass
