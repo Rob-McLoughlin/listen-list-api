f7fe307b7231aa7977d4655427c5cda5ea45d8cc
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2021-02-08 11:26:29 +0000

    feat(user): added sign in fn

--- utils_user.py
+++ utils_user.py
@@ -1,5 +1,6 @@
 import boto3
 import utils
+import json
 
 def sign_up(username: str, email: str, password: str):
   client = boto3.client('cognito-idp')
@@ -16,8 +17,25 @@ def sign_up(username: str, email: str, password: str):
   )
   print(response)
 
+def sign_in(username: str, password: str):
+  client = boto3.client('cognito-idp')
+  params = {
+    'USERNAME': username,
+    'PASSWORD': password
+  }
+  response = client.admin_initiate_auth(
+    UserPoolId=utils.keys('cog_pool_id'),
+    ClientId=utils.keys('cog_app_id'),
+    AuthFlow='ADMIN_NO_SRP_AUTH',
+    AuthParameters=params
+  )
+  return response
+  
 if __name__ == "__main__":
   email = 'robbiemcloughlin@gmail.com'
   username = 'rob2'
   pw = 'Password1@'
-  sign_up(username, email, pw)
+  # sign_up(username, email, pw)
+  response = sign_in(username, pw)
+  with open('key.json', 'w') as outfile:
+    json.dump(response, outfile, indent=2)
