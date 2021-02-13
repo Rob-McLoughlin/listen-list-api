94e196ff1b4b8338abacab1dfa4de191f9ee5e28
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2021-02-13 23:01:09 +0000

    feat(sy): retrieving token from cache

--- utils_spotify.py
+++ utils_spotify.py
@@ -9,17 +9,14 @@ from datetime import datetime
 import boto3
 
 
-def sy_get_token(client_id: str, client_secret: str) -> dict:
-    """Gets a Spotify token
-
-    Args:
-        client_id (str): Client ID
-        client_secret (str): Client secret
+def sy_get_token() -> dict:
+    """Gets a Spotify token using stored credentials
 
     Returns:
         dict: Response with tokens
     """
-
+    client_id = utils.keys('sy_client_id')
+    client_secret = utils.keys('sy_client_secret')
     url = "https://accounts.spotify.com/api/token"
     data = {"grant_type": "client_credentials"}
     r = requests.post(url, data, auth=HTTPBasicAuth(client_id, client_secret))
@@ -41,15 +38,13 @@ def sy_get_token(client_id: str, client_secret: str) -> dict:
         raise ValueError(f"{r.status_code}, {r.text}")
 
 
-def sy_check_token(token: dict) -> bool:
-    """Checks if a token dict is still in date
-
-    Args:
-        token (dict): The token object
+def sy_check_token() -> bool:
+    """Checks if the cached token dict is still in date
 
     Returns:
         bool: Whether the token is valid or not
     """
+    token = json.loads(utils.keys('sy_token'))
     expiry_sec = token["expires_in"]
     created_at = token["created_at"]
     new_timestamp = created_at + expiry_sec
@@ -77,7 +72,8 @@ def sy_search(term: str, key: dict, search_type="album,artist"):
 if __name__ == "__main__":
     client_id = utils.keys("sy_client_id")
     client_secret = utils.keys("sy_client_secret")
-    token = sy_get_token(client_id, client_secret)
+    token = sy_get_token()
+    print(sy_check_token())
     # print("Token", token)
     # results = sy_search('Kid A', token)
     # albums = [album for album in results['albums']['items']]
